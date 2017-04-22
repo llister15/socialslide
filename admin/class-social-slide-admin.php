5de891f45d95c9db13b15d136538d55a96fdaf31
Louis Lister <llister@wonkasoft.com>
2017-04-22 01:37:27 -0700

    added menus

--- admin/class-social-slide-admin.php
+++ admin/class-social-slide-admin.php
@@ -106,33 +106,32 @@ class Social_Slide_Admin {
 add_action( 'admin_menu', 'my_admin_menu' );
 
 function my_admin_menu() {
-	add_menu_page(
-		'Social Slide',
-		'Social Slide', 
-		'manage_options', 
-		'admin/index.php', 
-		'', 
-		'dashicons-rss', 99  );
-	  
-	  add_submenu_page ('admin/index.php',
+	add_menu_page('SocialSlide',
+		'SocialSlide', 
+		'manage_options',
+		'socialslide/admin/index.php', 
+		'',
+		'dashicons-rss',99);
+
+	 add_submenu_page ('socialslide/admin/index.php',
 	    'Settings',
 	    'Settings',
 	    'manage_options',
-	    'admin/index.php',
+	    'socialslide/admin/index.php',
 	    '');
 
-	  add_submenu_page ('admin/facebook.php',
+	  add_submenu_page ('socialslide/admin/index.php',
 	    'Facebook',
 	    'Facebook',
 	    'manage_options',
-	    'admin/index.php',
+	    'socialslide/admin/facebook.php',
 	    '');
 
-	  add_submenu_page ('admin/twitter.php',
+	  add_submenu_page ('socialslide/admin/index.php',
 	    'Twitter',
 	    'Twitter',
 	    'manage_options',
-	    'admin/index.php',
+	    'socialslide/admin/twitter.php',
 	    '');
 
 }
--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,47 @@
-<?php 
+<?php
+if (!current_user_can('manage_options')) {
+  wp_die(__('You do not have sufficient permissions to access this page.'));
+}
+?>
+<div class="wrap">
+    <?php screen_icon('themes'); ?> <h2>Front page elements</h2>
+ 
+    <form method="POST" action="">
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">
+                    <label for="num_elements">
+                        Number of elements on a row:
+                    </label>
+                </th>
+                <td>
+                    <input type="text" name="num_elements" size="25" />
+                </td>
+            </tr>                
+        </table>
+ 
+        <h3>Featured posts</h3>
+ 
+        <ul id="featured-posts-list">
+        </ul>
+       
+        <input type="hidden" name="element-max-id" />
+ 
+        <a href="#" id="add-featured-post">Add featured post</a>   
+    </form>
+     
+    <li class="front-page-element" id="front-page-element-placeholder"
+        style="display:none;">
+        <label for="element-page-id">Featured post:</label>
+        <select name="element-page-id">
+            <?php foreach ($posts as $post) : ?>
+                <option value="<?php echo $post->ID; ?>">
+                    <?php echo $post->post_title; ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+        <a href="#">Remove</a>
+    </li>
+ 
+</div>
 
-echo "hello";
